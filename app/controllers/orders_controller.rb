1badc28951766b00f529963c9b5dbdc915f57d15
tofutigerz <hfu3@ualberta.ca>
2022-05-22 07:43:13 -0600

    remove unused methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,9 +11,6 @@ class OrdersController < ApplicationController
 		@order = Order.find(params[:id])
 	end
 
-	# GET /orders/1/edit
-	def edit; end
-
 	# POST /orders or /orders.json
 	def create
 		@order = Order.new(total_cents: cart_subtotal_cents)
@@ -42,36 +39,6 @@ class OrdersController < ApplicationController
 					render json: @order.errors, status: :unprocessable_entity
 				end
 			end
-		end
-	end
-
-	# PATCH/PUT /orders/1 or /orders/1.json
-	def update
-		respond_to do |format|
-			if @order.update(order_params)
-				format.html do
-					redirect_to order_url(@order),
-					            notice: 'Order was successfully updated.'
-				end
-				format.json { render :show, status: :ok, location: @order }
-			else
-				format.html { render :edit, status: :unprocessable_entity }
-				format.json do
-					render json: @order.errors, status: :unprocessable_entity
-				end
-			end
-		end
-	end
-
-	# DELETE /orders/1 or /orders/1.json
-	def destroy
-		@order.destroy
-
-		respond_to do |format|
-			format.html do
-				redirect_to orders_url, notice: 'Order was successfully destroyed.'
-			end
-			format.json { head :no_content }
 		end
 	end
 
